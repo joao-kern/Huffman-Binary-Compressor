6427850cfb9fca793f2dc22569ab13067cb8cf0b
joao-kern <joaovictorramoskern@gmail.com>
2026-03-13 16:28:43 -0300

    Implementação CLI principal e fluxo de execução

--- main.cpp
+++ main.cpp
@@ -1,31 +1,49 @@
-#include "BitReader.h"
-#include "BitWriter.h"
+#include "Compressor.h"
+#include "Decompressor.h"
+#include <iostream>
+#include <filesystem>
+
+namespace fs = std::filesystem;
 
 int main(int argc, char const *argv[])
 {
-    BitWriter bit_writer(argv[1]);
-
-    for (int i = 0; i < 16; i++)
+    if (argc < 3)
     {
-        bit_writer.write_bit(i % 2);
+        std::cerr << "Insufficient number of arguments entered." << '\n';
+        return -1;
     }
 
-    bit_writer.close_file();
+    fs::path file_path = argv[1];
 
-    BitReader bit_reader(argv[1]);
+    std::string action = argv[2];
 
-    for (int i = 0; i < 16; i++)
+    try
     {
-
-        std::cout << bit_reader.read_bit();
-
-        if (((i + 1) % 8) == 0)
+        if (action == "compress")
         {
-            std::cout << std::endl;
+            std::cout << "Compressing File " + file_path.filename().string() + " ..." << std::endl;
+            Compressor *compressor = new Compressor(file_path);
+            delete compressor;
+            std::cout << "File Compressed Successfully!" << std::endl;
+        }
+        else if (action == "decompress")
+        {
+            std::cout << "Decompressing File " + file_path.filename().string() + " ..." << std::endl;
+            Decompressor *decompressor = new Decompressor(file_path);
+            delete decompressor;
+            std::cout << "File Decompressed Successfully!" << std::endl;
+        }
+        else
+        {
+            std::cerr << "Non-existent action." << '\n';
+            return -1;
         }
     }
-
-    bit_reader.close_file();
+    catch (const std::exception &e)
+    {
+        std::cerr << "Error: " << e.what() << '\n';
+        return -1;
+    }
 
     return 0;
 }
